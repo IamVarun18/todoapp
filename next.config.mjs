fe840ff783f16a613c62f63f089e39f0d6d2fcd2
IamVarun18 <130641848+IamVarun18@users.noreply.github.com>
2025-02-08 19:55:35 +0530

    new 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
 
-export default nextConfig;
+export default {
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve('./src');
+    return config;
+  },
+};
